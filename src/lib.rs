2b1cd3c69fd97f26721f94a0286b22ba89bbff5d
Arran <arran@arranfrance.com>
2020-03-22 09:02:01 +0000

    Fix: Clippy lints and update to 1.1
    This commit fixes various clippy lints.
    Fixes #1.
    Formats the file.
    Bumps log to 0.48
    Updates the crate to 1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,96 @@
-#[clippy::all]
+#![warn(clippy::all, clippy::pedantic)]
+#![allow(clippy::match_same_arms)]
 
 #[macro_use]
 extern crate log;
 extern crate pulldown_cmark;
 
-use pulldown_cmark::Event::{Text, SoftBreak, HardBreak, Start, End};
+use pulldown_cmark::Event::{End, HardBreak, SoftBreak, Start, Text};
 use pulldown_cmark::{Parser, Tag};
 
+#[must_use]
 pub fn strip_markdown(markdown: &str) -> String {
-    let mut parser = Parser::new(&markdown);
+    let parser = Parser::new(&markdown);
     let mut buffer = String::new();
-    while let Some(event) = parser.next() {
+
+    for event in parser {
         debug!("{:?}", event);
         match event {
-            Start(tag) => start_tag(tag, &mut buffer),
-            End(tag) => end_tag(tag, &mut buffer),
-            Text(text) => { debug!("Pushing {}", &text); buffer.push_str(&text); },
-            SoftBreak => fresh_line(&mut buffer),
-            HardBreak => fresh_line(&mut buffer),
-            _ => ()
+            Start(tag) => start_tag(&tag, &mut buffer),
+            End(tag) => end_tag(&tag, &mut buffer),
+            Text(text) => {
+                debug!("Pushing {}", &text);
+                buffer.push_str(&text);
+            }
+            SoftBreak | HardBreak => fresh_line(&mut buffer),
+            _ => (),
         }
     }
     buffer
 }
 
-fn start_tag(tag: Tag, buffer: &mut String) {
-        match tag {
-            Tag::Paragraph => (),
-            Tag::Rule => fresh_line(buffer),
-            Tag::Header(_level) => (),
-            Tag::Table(_alignments) => (),
-            Tag::TableHead => (),
-            Tag::TableRow => (),
-            Tag::TableCell => (),
-            Tag::BlockQuote => (),
-            Tag::CodeBlock(_info) => fresh_hard_break(buffer),
-            Tag::List(_number) => fresh_line(buffer),
-            Tag::Item => (),
-            Tag::Emphasis => (),
-            Tag::Strong => (),
-            Tag::Code => (),
-            Tag::Link(_dest, title) => {
-                if !title.is_empty() {
-                    buffer.push_str(&title);
-                }
+fn start_tag(tag: &Tag, buffer: &mut String) {
+    match tag {
+        Tag::CodeBlock(_info) => fresh_hard_break(buffer),
+        Tag::List(_number) => fresh_line(buffer),
+        Tag::Link(_dest, title) | Tag::Image(_dest, title) => {
+            if !title.is_empty() {
+                buffer.push_str(&title);
             }
-            Tag::Image(_dest, title) => {
-                if !title.is_empty() {
-                    buffer.push_str(&title);
-                }
-            }
-            Tag::FootnoteDefinition(_) => (),
         }
+        Tag::Rule => fresh_line(buffer),
+        Tag::Paragraph => (),
+        Tag::Header(_level) => (),
+        Tag::Table(_alignments) => (),
+        Tag::TableHead => (),
+        Tag::TableRow => (),
+        Tag::TableCell => (),
+        Tag::BlockQuote => (),
+        Tag::Item => (),
+        Tag::Emphasis => (),
+        Tag::Strong => (),
+        Tag::Code => (),
+        Tag::FootnoteDefinition(_) => (),
     }
+}
 
-    fn end_tag(tag: Tag, buffer: &mut String) {
-        match tag {
-            Tag::Paragraph => (),
-            Tag::Rule => (),
-            Tag::Header(_) => fresh_line(buffer),
-            Tag::Table(_) => {
-                fresh_line(buffer);
-            },
-            Tag::TableHead => {
-                fresh_line(buffer);
-            },
-            Tag::TableRow => {
-                fresh_line(buffer);
-            },
-            Tag::BlockQuote => fresh_line(buffer),
-            Tag::CodeBlock(_) => fresh_line(buffer),
-            Tag::List(_) => (),
-            Tag::Item => fresh_line(buffer),
-            Tag::Image(_, _) => (), // shouldn't happen, handled in start
-            Tag::FootnoteDefinition(_) => (),
-            _ => ()
+fn end_tag(tag: &Tag, buffer: &mut String) {
+    match tag {
+        Tag::Paragraph => (),
+        Tag::Rule => (),
+        Tag::Header(_) => fresh_line(buffer),
+        Tag::Table(_) => {
+            fresh_line(buffer);
         }
+        Tag::TableHead => {
+            fresh_line(buffer);
+        }
+        Tag::TableRow => {
+            fresh_line(buffer);
+        }
+        Tag::BlockQuote => fresh_line(buffer),
+        Tag::CodeBlock(_) => fresh_line(buffer),
+        Tag::List(_) => (),
+        Tag::Item => fresh_line(buffer),
+        Tag::Image(_, _) => (), // shouldn't happen, handled in start
+        Tag::FootnoteDefinition(_) => (),
+        _ => (),
     }
+}
 
-    fn fresh_line(buffer: &mut String) {
-        debug!("Pushing \\n");
-        buffer.push('\n');
-    }
+fn fresh_line(buffer: &mut String) {
+    debug!("Pushing \\n");
+    buffer.push('\n');
+}
 
-    fn fresh_hard_break(buffer: &mut String) {
-        debug!("Pushing \\n\\n");
-        buffer.push_str("\n\n");
-    }
-
+fn fresh_hard_break(buffer: &mut String) {
+    debug!("Pushing \\n\\n");
+    buffer.push_str("\n\n");
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
     #[test]
     fn basic_inline_strong() {
         let markdown = r#"**Hello**"#;
@@ -131,7 +129,6 @@ Header
         assert_eq!(strip_markdown(markdown), expected);
     }
 
-    
     #[ignore]
     #[test]
     fn strikethrough() {
@@ -150,23 +147,23 @@ Header
 4. And another item.
 "#;
 
-let expected = r#"
+        let expected = r#"
 First ordered list item
 Another item
 Actual numbers don't matter, just that it's a number
 Ordered sub-list
 And another item.
 "#;
- assert_eq!(strip_markdown(markdown), expected);
+        assert_eq!(strip_markdown(markdown), expected);
     }
 
     #[test]
     fn basic_list() {
-    let markdown = r#"
+        let markdown = r#"
 * alpha
 * beta
 "#;
-    let expected = r#"
+        let expected = r#"
 alpha
 beta
 "#;
@@ -175,11 +172,11 @@ beta
 
     #[test]
     fn list_with_header() {
-    let markdown = r#"# Title
+        let markdown = r#"# Title
 * alpha
 * beta
 "#;
-    let expected = r#"Title
+        let expected = r#"Title
 
 alpha
 beta
@@ -233,7 +230,7 @@ alert(s);
     }
 
     #[test]
-    fn block_quote(){
+    fn block_quote() {
         let markdown = r#"> Blockquotes are very handy in email to emulate reply text.
 > This line is part of the same quote."#;
         let expected = "Blockquotes are very handy in email to emulate reply text.
